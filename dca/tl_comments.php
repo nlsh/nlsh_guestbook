ecc0fdc9bff9faa89170cb684631c2a465a79181
Nils Heinold <github@nilsheinold.de>
2013-03-11 19:28:56 +0100

    Datum der Eintragung editieren

---
+++ dca/tl_comments.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Erweiterung des tl_comments DCA`s
+ *
+ * @copyright  Nils Heinold 2013
+ * @author     Nils Heinold
+ * @package    nlshGuestbook
+ * @link       http://github.com/nlsh/nlsh_guestbook
+ * @license    LGPL
+ */
+
+
+/**
+* Table tl_comments
+*/
+
+foreach($GLOBALS['TL_DCA']['tl_comments']['palettes'] as $k => $v)
+{
+        if($k != '__selector__')    {
+                                        if(strstr($v, '{comment_legend},comment;'))
+                                            $GLOBALS['TL_DCA']['tl_comments']['palettes'][$k] = str_replace('{comment_legend},comment;', '{comment_legend},date;comment;', $v);
+                                    }
+}
+
+$GLOBALS['TL_DCA']['tl_comments']['fields']['date']['inputType'] =  'text';
+$GLOBALS['TL_DCA']['tl_comments']['fields']['date']['eval']      =  array
+                                                                    (
+                                                                        'rgxp'=>'date', 'datepicker'=>$this->getDatePickerString(),'tl_class'=>'w50 wizard'
+                                                                    );
