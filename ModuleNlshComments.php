b19fa9047f9139a451c62210614c961a09e57c60
Nils Heinold <github@nilsheinold.de>
2013-04-16 19:02:32 +0200

    class ModuleNlshComments komplett neu

--- ModuleNlshComments.php
+++ ModuleNlshComments.php
@@ -36,11 +36,37 @@
  * @author     Nils Heinold
  * @package    nlsh_guestbook
  */
-class ModuleNlshComments extends ModuleComments
+class ModuleNlshComments extends \Module
 {
 
-    // Definition der Smilies in einem Array
-    public $arrSmilies = array
+    /**
+    * Template
+    * @var string
+    */
+    protected $strTemplate = 'nlsh_guestbook_default';
+
+
+    /**
+    * Neueintrag für Gästebuch.
+    * Handelt es sich um einen Neueintrag ins Gästebuch?
+    *
+    * @var boolean
+    */
+    protected $inputNewEntrie = false;
+
+
+    /**
+     * Definition des GET- Namens zur Abfrage des Gästebuch- Modules
+     */
+    const GET_INPUT_GBENTRIE = 'nlsh_gb_input';
+
+
+    /**
+     * Definition der Smilies in einem Array
+     *
+     * Definition der Smilies in einem Array
+     */
+    protected $arrSmilies = array
     (
         array ('[smile]:D[/smile]',        '', 'big.gif'),
         array ('[smile]:)[/smile]',        '', 'smile.gif'),
@@ -66,18 +92,50 @@ class ModuleNlshComments extends ModuleComments
     );
 
 
-    // Get- Parameter, um einen Eintrag zu erzeugen
-    const GET_INPUT_GBENTRIE = 'nlsh_gb_input';
-
-
+    /**
+    * Display a wildcard in the back end
+    * @return string
+    */
     public function generate()
     {
-        // die gbentries.txt importieren
-        //$this->importGbEntries('tl_files/');
+    if (TL_MODE == 'BE')
+    {
+        $objTemplate = new \BackendTemplate('be_wildcard');
 
-        // Die Originalmethode ausführen
-        $strOutput = parent::generate();
+        $objTemplate->wildcard = '### nlsh_guestbook ###';
+        $objTemplate->title = $this->headline;
+        $objTemplate->id = $this->id;
+        $objTemplate->link = $this->name;
+        $objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
 
+        return $objTemplate->parse();
+    }
+
+    return parent::generate();
+    }
+
+
+    /**
+    * Generate the module
+    */
+    protected function compile()
+    {
+    global $objPage;
+
+        $this->import('Comments');
+        $objConfig = new \stdClass();
+
+        $objConfig->perPage        = $this->perPage;
+        $objConfig->order          = $this->com_order;
+        $objConfig->template       = $this->com_template;
+        $objConfig->requireLogin   = $this->com_requireLogin;
+        $objConfig->disableCaptcha = $this->com_disableCaptcha;
+        $objConfig->bbcode         = $this->com_bbcode;
+        $objConfig->moderate       = $this->com_moderate;
+
+        $this->Comments->addCommentsToTemplate($this->Template, $objConfig, 'tl_page', $objPage->id, $GLOBALS['TL_ADMIN_EMAIL']);
+
+        // Sprache nachladen
         $this->loadLanguageFile('tl_style');
 
         // BBCode definieren
@@ -90,33 +148,49 @@ class ModuleNlshComments extends ModuleComments
             array ('[quote]','[/quote]', 'comment_add.png', $GLOBALS['TL_LANG']['nlsh_guestbook']['insertQoute']),
         );
 
-        // Template wechseln
-        $this->strTemplate = $this->com_nlsh_gb_template;
-        $this->Template    = new FrontendTemplate($this->strTemplate);
+        // Anzahl aller Einträge ermitteln, um Nummer des Eintrages festlegen zu können
+        $arrGbEntries = $this->Database->prepare("SELECT * FROM tl_comments WHERE source=? AND parent=?" . (!BE_USER_LOGGED_IN ? " AND published=1" : "") . " ORDER BY date ASC")
+                    ->execute('tl_page', $objPage->id)
+                    ->fetchAllAssoc();
 
-        // Template mit Standardwerte vorblegen
-        $this->Template->headline                            = $this->headline;
-        $this->Template->hl                                  = $this->hl;
+        // Text für Anzahl der Einträge eintragen
+        $this->Template->howManyEntriesText = sprintf($GLOBALS['TL_LANG']['nlsh_guestbook']['howManyEntries'], count($arrGbEntries));
 
-        if ($this->cssID[0] == true) $this->Template->cssID  = ' id ="' . $this->cssID[0] . '"';
+        // die vom nlsh_gb_initial- Template übergebenen Comments- Daten wieder in ein Array umwandeln
+        // und die Nummer hinzufügen
+        foreach( $this->Template->comments as $comment)
+        {
+            $tempComment = unserialize(trim($comment));
 
-        $this->Template->class                               = 'mod_nlsh_guestbook';
-        if ($this->cssID[1] == true) $this->Template->class .= ' ' . $this->cssID[1];
+            // Durchnummerieren
+            for ($a = 0; $a < count($arrGbEntries); $a++)
+            {
+                if ($arrGbEntries[$a]['id'] == $tempComment['id'])
+                {
+                    $tempComment['nr'] = $a + 1;
+                }
+            }
 
-        $this->Template->style = false;
-        $this->Template->style = ($this->space[0] == true) ? 'margin-top:' . $this->space[0] . 'px;' : '';
-        $this->Template->style .= ($this->space[1] == true) ? 'margin-bottom:' . $this->space[1] . 'px;' : '';
+            //Überschriften herausholen
+            if (strpos($tempComment['comment'], '[/h]'))
+            {
+                $start = strpos($tempComment['comment'], '[h]');
+                $end   = strpos($tempComment['comment'], '[/h]') + 4;
 
-        // String zerlegen und Übergabe in das Template
-        $templateVars = $this->_getTemplateVars($strOutput);
+                $tempComment['headlineComment'] = substr($tempComment['comment'], $start + 3 , $end - $start - 7);
+                $tempComment['comment'] = substr($tempComment['comment'], 0, $start) . substr($tempComment['comment'], $end);
+            }
 
-        $this->Template->countCommentsText = $templateVars['countCommentsText'];
-        $this->Template->form              = $templateVars['form'];
-        $this->Template->pagination        = $templateVars['pagination'];
-        $this->Template->countComments     = $templateVars['countComments'];
-        $this->Template->gbEntries         = $templateVars['gbEntries'];
+            $tempArrComments[] = $tempComment;
+        }
 
-        // Wenn Link zum neuem Gästebucheintrag benutzt wurde
+        // und in das Template übernehmen
+        $this->Template->comments = $tempArrComments;
+
+        // Anzahl der übermittelten Kommentare ins Template
+        $this->Template->countComments = count($this->Template->comments);
+
+        // Wenn Link für ein neuen Gästebucheintrag benutzt wurde
         if ($this->Input->get(self::GET_INPUT_GBENTRIE) === 'true')
         {
             $this->Template->inputNewEntrie = true;
@@ -125,151 +199,43 @@ class ModuleNlshComments extends ModuleComments
         // Html- Link für neuen Eintrag erzeugen
         $this->Template->htmlLinkNewEntrie = $this->getHtmlLinkForNewNlshGbEntrie();
 
-        //Templateausgabe
-        return $this->Template->parse();
-    }
-
-
-    /**
-     * Zerlegung des Initialstrings
-     * @param string HTML- Ausgabestring des 'nlsh_gb_initial'- Templates
-     * @return array Array mit den Daten
-     */
-    protected function _getTemplateVars($strFromInitialTemplate)
-    {
-        global $objPage;
-
-        // zum Abbruch der Eingabe, Button erzeugen
-        $this->cancelButton        = new \FrontendTemplate('nlsh_cancelButton');
-        $this->cancelButton->value = $GLOBALS['TL_LANG']['nlsh_guestbook']['cancelButton'];
-        $this->cancelButton        = $this->cancelButton->parse();
-
-        // Formulareingabe extrahieren
-        $start = strpos($strFromInitialTemplate, '<form ');
-        $end   = strpos($strFromInitialTemplate, '</form>');
-
-
-        // Formular wieder zusammensetzen
-        $form  = "<!-- indexer::stop -->\n<div class=\"form\">\n";
-        $form .= substr($strFromInitialTemplate, $start, $end - $start);
-
-        // letztes <\div> entfernen
-        $form  = substr($form, 0, strripos($form, '</div>'));
-
-        // Cancel- Button einfügen
-        $form .= $this->cancelButton;
-
-        // und schließen
-        $form .= "\n</div>\n</form>\n</div>\n<!-- indexer::continue -->\n";
-
-        // self::GET_INPUT_GBENTRIE auf false setzen, damit nach Eingabe eines Eintrages das Gästebuch wieder angezeigt wird
-        // siehe #3
-        $form = str_replace(self::GET_INPUT_GBENTRIE . '=true', self::GET_INPUT_GBENTRIE . '=false', $form);
-
         // Formular erweitern mit Auswahl Smilies und BBCode und Überschriftenfeld
         if ( $this->com_bbcode == true)
         {
-            $bbCode  = $this->_getHtmlDivSelectBbcOrSmilies('bbcode', 'com_tl_page_' . $objPage->id, $this->arrBbcFormat);
-            $smilies = $this->_getHtmlDivSelectBbcOrSmilies('smilies', 'com_tl_page_' . $objPage->id, $this->arrSmilies);
-
-            $form = str_replace('<div class="submit_container">', $bbCode . $smilies . "\n" . '<div class="submit_container">', $form);
+            $bbCode  = $this->getHtmlDivSelectBbcOrSmilies('bbcode', 'com_tl_page_' . $objPage->id, $this->arrBbcFormat);
+            $smilies = $this->getHtmlDivSelectBbcOrSmilies('smilies', 'com_tl_page_' . $objPage->id, $this->arrSmilies);
         }
 
-        $headline = "<div class =\"widget\">\n"
-                    . "  <input id=\"ctrl_headline\" class=\"text\" type=\"text\" maxlength=\"255\" value=\"\" name=\"headline\" />"
-                    . " <label for=\"ctrl_headline\"><span class=\"headline\">" . $GLOBALS['TL_LANG']['nlsh_guestbook']['headline'] . "</span></label>"
-                    . "\n</div>\n";
+        // Formular für Dateneingabe vorbereiten
+        $this->form                = new \FrontendTemplate('nlsh_mod_comment_form');
+        $this->form->requireLogin  = $this->Template->requireLogin;
+        $this->form->confirm       = $this->Template->confirm;
+        $this->form->allowComments = $this->Template->allowComments;
+        $this->form->action        = str_replace(self::GET_INPUT_GBENTRIE . '=true', self::GET_INPUT_GBENTRIE . '=false', $this->Template->action);
+        $this->form->formId        = $this->Template->formId;
+        $this->form->bbCode        = $bbCode;
+        $this->form->smilies       = $smilies;
+        $this->form->submit        = $this->Template->submit;
+        $this->form->fields        = $this->Template->fields;
+        $this->form->cancelValue   = $GLOBALS['TL_LANG']['nlsh_guestbook']['cancelButton'];
+        $this->form->hasError      = $this->Template->hasError;
+        $this->Template->form      = $this->form->parse();
 
-        $startHeadline = strpos($form, '<textarea') - 23;
-
-        $form = substr($form, 0, $startHeadline) . $headline  . substr($form, $startHeadline);
-
-        $return['form'] = $form;
-
-        // Array erzeugen
-        $arrData = explode("^", $strFromInitialTemplate);
-        $count   = count($arrData);
-
-        // Wenn nur ein Feld, dann keine Daten, Mitteilung und zurück
-        if($count == 1)
-        {
-            $return['countCommentsText'] = sprintf($GLOBALS['TL_LANG']['nlsh_guestbook']['howManyEntries'], 0);
-
-            return $return;
-        }
-
-        ;
-        // Pagination des Ausgabecontainers, falls vorhanden
-        if (strpos($strFromInitialTemplate, '<div class="pagination') != false )
-        {
-            $start                = strpos($strFromInitialTemplate, '<div class="pagination');
-            $end                  = strpos($strFromInitialTemplate, '</ul>') + 5;
-            $return['pagination'] = "\n<!-- indexer::stop -->\n" . substr($strFromInitialTemplate, $start, $end - $start) . "\n\n </div>\n<!-- indexer::continue -->\n";
-        }
-
-        // Alle Gästebucheinträge holen zum Nummerieren der Einträge benötigt
-        $arrGbEntries = $this->Database->prepare("SELECT * FROM tl_comments WHERE source=? AND parent=?" . (!BE_USER_LOGGED_IN ? " AND published=1" : "") . " ORDER BY date ASC")
-                    ->execute('tl_page', $objPage->id)
-                    ->fetchAllAssoc();
-
-        $return['countCommentsText'] = sprintf($GLOBALS['TL_LANG']['nlsh_guestbook']['howManyEntries'], count($arrGbEntries));
-
-
-        // Datensätze auslesen
-        $newArr = array();
-
-        for ($i = 0; $i < $count; $i++)
-        {
-            // Erster Eintrag und letzter Eintrag kann weg
-            if ( ($i != 0) && ($i != $count))
-            {
-                $arrTemp = unserialize($arrData[$i]);
-
-                // leere Einträge ignorieren
-                if ($arrTemp['name'] == true)
-                {
-                    // Durchnummerieren
-                    for ($a = 0; $a < count($arrGbEntries); $a++)
-                    {
-                        if ($arrGbEntries[$a]['id'] == $arrTemp['id'])
-                        {
-                            $arrTemp['nr'] = $a + 1;
-                        }
-                    }
-
-                    //Überschriften herausholen
-                    if (strpos($arrTemp['comment'], '[/h]'))
-                    {
-                        $start = strpos($arrTemp['comment'], '[h]');
-                        $end   = strpos($arrTemp['comment'], '[/h]') + 4;
-
-                        $arrTemp['headlineComment'] = substr($arrTemp['comment'], $start + 3 , $end - $start - 7);
-                        $arrTemp['comment'] = substr($arrTemp['comment'], 0, $start) . substr($arrTemp['comment'], $end);
-                    }
-
-                    // und übergeben
-                    $newArr[] = $arrTemp;
-                };
-            }
-        }
-
-        $return['countComments']  = count($newArr);
-        $return['gbEntries']      = $newArr;
-
-        return $return;
     }
 
 
     /**
-     * HTML- String zur Auswahl der BB- Codes und Smilies
-     * @param string CSS- Klasse für Div- Container
-     * @param array Smiliebeschreibungen/BBCodebeschreibung
-     * @return string Html für Auswahl- Container
+     * HTML- String zur Auswahl der BB- Codes und Smilies erzeugen
+     *
+     * @param  string  CSS- Klasse für Div- Container
+     * @param  string  Name des CSS-ID`s des Formulares, in dem sich die Textarea befindet
+     * @param  array   Smiliebeschreibungen/BBCodebeschreibung
+     * @return string  Html für Auswahl- Container
      */
-     protected function _getHtmlDivSelectBbcOrSmilies ($strClass, $strIdForm, $arrArray)
+     public function getHtmlDivSelectBbcOrSmilies ($strClass, $strCssIdForm, $arrArray)
      {
         $return  = '<div class="' . $strClass . '">';
-        $return .= $this->_getHtmlLinkInsertWithJava($strIdForm, $arrArray);
+        $return .= $this->getHtmlLinkInsertWithJava($strCssIdForm, $arrArray);
         $return .= "</div>\n";
 
         return $return;
@@ -278,19 +244,20 @@ class ModuleNlshComments extends ModuleComments
 
 
     /**
-     * Erstellt das Html für einen Link für die Auswahl des BBCodes/ Smilies
-     * @param string Name des Id`s des Formulares, in dem sich die Textarea befindet
-     * @param array Array mit den Smiliebeschreibungen/BBCodebeschreibung
-     * @return string HTML für den Link
+     * Erstellt das HTML für einen Link für die Auswahl des BBCodes/ Smilies
+     *
+     * @param  string  Name des CSS- Id`s des Formulares, in dem sich die Textarea befindet
+     * @param  array   Array mit den Smiliebeschreibungen/BBCodebeschreibung
+     * @return string  HTML für den Link
      */
-    protected function _getHtmlLinkInsertWithJava($strFormId, $arrArray)
+    public function getHtmlLinkInsertWithJava($strCssFormId, $arrArray)
     {
         $strUrl  = 'system/modules/nlsh_guestbook/html/smilies/';
         $strHtml = '';
 
         for ($i = 0; $i < count($arrArray); $i++)
         {
-            $strHtml .= "\n<a href=\"javascript:insert('$strFormId', ' " . $arrArray[$i][0] . " ', ' " . $arrArray[$i][1] . " ')\"><img src=\"" . $strUrl . $arrArray[$i][2] . "\" alt=\"" . $arrArray[$i][0] . "\" title=\"" . $arrArray[$i][3] . "\" /></a>";
+            $strHtml .= "\n<a href=\"javascript:insert('$strCssFormId', ' " . $arrArray[$i][0] . " ', ' " . $arrArray[$i][1] . " ')\"><img src=\"" . $strUrl . $arrArray[$i][2] . "\" alt=\"" . $arrArray[$i][0] . "\" title=\"" . $arrArray[$i][3] . "\" /></a>";
         }
 
         return $strHtml;
@@ -298,8 +265,9 @@ class ModuleNlshComments extends ModuleComments
 
 
     /**
-     * HTML- String für Link zum neuen Eintrag erzeugen
-     * @return string HTML- String für Link zum neuen Eintrag
+     * HTML- String für Link zum neuen Gästebucheintrag erzeugen
+     *
+     * @return string  HTML- String für Link zum neuen Eintrag
      */
     public function getHtmlLinkForNewNlshGbEntrie()
     {
